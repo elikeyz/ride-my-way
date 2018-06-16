bcc26e2e44d1e68cdf12560099a2f2b47f2f14af
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-06-16 23:20:55 +0100

    create endpoint to get specific ride offers

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,20 @@ const fs = require('fs');
 
 const appRouter = app => {
     app.get('/v1/rides', (req, res) => {
-        fs.readFile( './models/rides.json', 'utf8', (err, data) => {
+        fs.readFile('./models/rides.json', 'utf8', (err, data) => {
             console.log(data);
             res.end(data);
         });
-    })    
+    })
+
+    app.get('/v1/rides/:id', (req, res) => {
+        fs.readFile('./models/rides.json', 'utf8', (err, data) => {
+            const rides = JSON.parse(data);
+            const ride = rides[`ride${req.params.id}`];
+            console.log(ride);
+            res.end(JSON.stringify(ride));
+        })
+    })
 }
 
 module.exports = appRouter;
