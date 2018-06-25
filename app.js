d88fc96b93a23939debe1808b587c3ea440121d1
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-06-25 22:38:22 +0100

    add environmental port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const routes = require('./routes/routes.js');
 
 routes(app);
 
-const server = app.listen(8081, () => {
+const server = app.listen((process.env.PORT || 8081), () => {
     const host = server.address().address;
     const port = server.address().port;
 
