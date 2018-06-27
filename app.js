4cbbe3c2304447b8ec006013d5d45d7af3517e79
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-06-27 11:24:49 +0100

    implement es6 corrections

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-const express = require('express');
+import express from 'express';
+import bodyParser from 'body-parser';
+import routes from './utils/routes';
+
 const app = express();
-const routes = require('./routes/routes.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 routes(app);
 
 const server = app.listen((process.env.PORT || 8081), () => {
-    const host = server.address().address;
-    const port = server.address().port;
+  const host = server.address().address;
+  const { port } = server.address();
+  console.log(`Example app listening at http:${host}${port}`);
+});
 
-    console.log(`Example app listening at http:${host}${port}`);
-})
-
-module.exports = app;
+export default app;
