1b8e560e003dec4c93c16804060c8773b2202b30
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-06-28 11:29:47 +0100

    use cross-env to enable cross-device compatibility in start script

--- utils/routes.js
+++ utils/routes.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 
 const routes = (app) => {
   app.get('/', (req, res) => {
-    res.status(200).send('Welcome to the Ride My Way API, version 1! \nNavigate to /rides to get all rides. \nNavigate to /rides/{id} with ids 1, 2, and 3 to get each corresponding ride. \nRun POST requests on /rides to add a sample ride and on /rides/{id}/requests to create a sample request on any of the rides.');
+    res.status(200).send('Welcome to the Ride My Way API, version 1! \nNavigate to api/v1/rides to get all rides. \nNavigate to api/v1/rides/{id} with ids 1, 2, and 3 to get each corresponding ride. \nRun POST requests on api/v1/rides to add a sample ride and on api/v1/rides/{id}/requests to create a sample request on any of the rides.');
   });
 
   app.get('/api/v1/rides', (req, res) => {
