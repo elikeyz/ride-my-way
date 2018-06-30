3ae80e06118bf4ef4736bbabbd24da25867db965
Elijah Enuem-Udogu <koppter.kom@gmail.com>
2018-06-30 22:11:58 +0100

    add headers to response objects

--- controllers/rides.js
+++ controllers/rides.js
@@ -2,7 +2,11 @@ import fs from 'fs';
 
 export function getAllRides(req, res) {
   fs.readFile('./models/rides.json', 'utf8', (err, data) => {
-    res.status(200).send(data);
+    res.status(200).send({
+      message: 'Rides gotten successfully',
+      success: true,
+      body: data,
+    });
   });
 }
 
@@ -10,7 +14,11 @@ export function getARide(req, res) {
   fs.readFile('./models/rides.json', 'utf8', (err, data) => {
     const rides = JSON.parse(data);
     const ride = rides[`ride${req.params.id}`];
-    res.status(200).send(JSON.stringify(ride));
+    res.status(200).send({
+      message: 'Ride gotten successfully',
+      success: true,
+      body: JSON.stringify(ride),
+    });
   });
 }
 
@@ -28,7 +36,11 @@ export function addRide(req, res) {
   fs.readFile('./models/rides.json', 'utf8', (err, data) => {
     const rides = JSON.parse(data);
     rides.ride4 = newRide.ride4;
-    res.status(200).send(JSON.stringify(rides));
+    res.status(201).send({
+      message: 'Ride added successfully',
+      success: true,
+      body: JSON.stringify(rides),
+    });
   });
 }
 
@@ -37,6 +49,10 @@ export function addRequest(req, res) {
     const rides = JSON.parse(data);
     const ride = rides[`ride${req.params.id}`];
     ride.requests = { name: 'passenger' };
-    res.status(200).send(JSON.stringify(rides));
+    res.status(201).send({
+      message: 'Ride requested successfully',
+      success: true,
+      body: JSON.stringify(rides),
+    });
   });
 }
